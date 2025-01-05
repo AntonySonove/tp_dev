755529e9bbd6a86701871a6bde3c9428df10b1aa
antony <asonove@gmail.com>
2025-01-05 16:27:43 +0100

    save 05_01_25

--- projet fil rouge/html css js/script.js
+++ projet fil rouge/html css js/script.js
@@ -1,24 +1,34 @@
 // Dropdown menu créer et gérer
 
-    //Dropdown créer
+    //Dropdown créer et gérer
+
+        //créer
 
 const dropdownMenuCreer= document.getElementById("dropdownMenuCreer");
 const showMenuCreer= document.getElementById("showMenuCreer");
 
-dropdownMenuCreer.addEventListener("click",function(){
-    showMenuCreer.classList.toggle("menuCache");
-});
+dropdownMenuCreer.onmouseenter=(function(){mouseEnterCreer()});
+dropdownMenuCreer.onmouseleave=(function(){mouseLeaveCreer()});
 
-    //Dropdown gérer
+function mouseEnterCreer(){
+    showMenuCreer.classList.add("menuAffiche");
+}
+function mouseLeaveCreer(){
+    showMenuCreer.classList.remove("menuAffiche")
+    // showMenuCreer.classList.add("menuCache");
+}
+        //gérer
 
 const dropdownMenuGerer= document.getElementById("dropdownMenuGerer");
 const showMenuGerer= document.getElementById("showMenuGerer");
 
-dropdownMenuGerer.addEventListener("click",function(){
-    showMenuGerer.classList.toggle("menuCache");
-});
+dropdownMenuGerer.onmouseenter=(function(){mouseEnterGerer()});
+dropdownMenuGerer.onmouseleave=(function(){mouseLeaveGerer()});
 
-// document.addEventListener("click",function(){
-//     console.log("click");
-//     showMenuCreer.classList.add("menuCache");
-// })
+function mouseEnterGerer(){
+    showMenuGerer.classList.add("menuAffiche");
+}
+function mouseLeaveGerer(){
+    showMenuGerer.classList.remove("menuAffiche")
+    // showMenuGerer.classList.add("menuCache");
+}
